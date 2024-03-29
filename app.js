929b0b44849d7b4213aa129003df83589e70a658
David Villaron <90426008+David-V1@users.noreply.github.com>
2024-03-29 16:40:54 -0500

    Refactor image/content bullet point selection logic and bullet point placement. Working bullet points

--- app.js
+++ app.js
@@ -1,14 +1,30 @@
 const cardImages = document.querySelectorAll(".card-image-container .card-img");
-const cardContent = document.querySelectorAll(
-	".card-content-container .card-content"
-);
-const topicIcon = document.querySelectorAll(".card-content .icon-wrapper");
-const mobileCarouselContainer = document.querySelector(
-	"mobile-carousel-container"
-);
+const cardContents = document.querySelectorAll(".card-content-container .card-content");
+const mobileCarouselContainer = document.querySelector("#mobile_carousel-container");
+const bulletPointsContainer = document.querySelector(".bullet-points-btns-container");
+
+function generateBulletPointsButtons(container) {
+	if (cardContents.length === cardImages.length) {
+		for (let i = 0; i < cardContents.length; i++) {
+			const bulletPointButton = document.createElement("button");
+			const bulletPointDiv = document.createElement("div");
+
+			bulletPointButton.classList.add("bullet-point", "topic-btn");
+			bulletPointButton.setAttribute("card-topic", i + 1);
+			bulletPointDiv.classList.add("icon-wrapper", "icon-circle-o");
+			bulletPointButton.appendChild(bulletPointDiv);
+			container.appendChild(bulletPointButton);
+		}
+	} else {
+		console.error("Something went wrong. cardContents.length !== cardImages.length");
+	}
+}
+generateBulletPointsButtons(bulletPointsContainer);
 
 function selectedIcon(topicNum) {
+	const topicIcon = document.querySelectorAll(".icon-wrapper");
 	topicIcon.forEach((icon) => {
+		console.log("icon", icon);
 		icon.classList.add("icon-circle-o");
 		icon.classList.remove("icon-circle");
 		const cardTopic = icon.parentElement.getAttribute("card-topic");
@@ -18,23 +34,23 @@ function selectedIcon(topicNum) {
 		}
 	});
 }
+selectedTopic();
 
 function showFirstTopic() {
 	//icon
+	const topicIcon = document.querySelectorAll(".icon-wrapper");
 	topicIcon.forEach((icon, idx) => {
-		icon.classList.add("icon-circle-o");
-		icon.classList.remove("icon-circle");
-		if (idx === 0) {
-			icon.classList.remove("icon-circle-o");
-			icon.classList.add("icon-circle");
-		}
+		icon.classList.toggle("icon-circle", idx === 0);
+		icon.classList.toggle("icon-circle-o", idx !== 0);
 	});
-	// image
-	cardImages.forEach((cardImage, idx) => {
-		if (idx !== 0) {
-			cardImage.style.display = "none";
-		}
-	});
+	// A helper function to toggle display based on index
+	const toggleDisplay = (elements) => {
+		elements.forEach((element, idx) => {
+			element.style.display = idx === 0 ? "" : "none";
+		});
+	};
+	toggleDisplay(cardImages);
+	toggleDisplay(cardContents);
 }
 
 showFirstTopic();
@@ -50,21 +66,38 @@ function showCardImage(topicNum) {
 	}
 }
 
+function showCardImageContent(topicNum) {
+	cardContents.forEach((card) => {
+		card.style.display = "none";
+	});
+	const imageContent = document.querySelector(`.card-content[card-topic="${topicNum}"]`);
+	if (imageContent) {
+		imageContent.style.display = "block";
+	}
+}
+
 function selectedTopic() {
-	cardContent.forEach((card) => {
-		const button = card.querySelector(".icon-wrapper");
-		button.addEventListener("click", (e) => {
-			// holds topic #
-			const parentElement = e.target.closest(".card-content");
-			const topicNum = parentElement.getAttribute("card-topic");
+	const bulletPoints = document.querySelectorAll(".bullet-point");
+	bulletPoints.forEach((btn) => {
+		btn.addEventListener("click", (e) => {
+			const topicNum = btn.getAttribute("card-topic");
 			if (topicNum) {
 				selectedIcon(topicNum);
 				showCardImage(topicNum);
+				showCardImageContent(topicNum);
 			}
 		});
 	});
 }
 
-selectedTopic();
+// selectedTopic();
 
-function mobileCarousel() {}
+function mobileCarousel() {
+	console.log("mobileCarousel");
+	// change div background color of mobileCarouselContainer
+	// icon.classList.add("icon-circle-o"); to mobileCarouselContainer
+	// icon.classList.remove("icon-circle"); to mobileCarouselContainer
+	// mobileCarouselContainer.classList.add("icon-circle-o");
+}
+
+// mobileCarousel();
